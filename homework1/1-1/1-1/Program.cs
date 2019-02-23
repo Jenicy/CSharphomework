12114f4052635e57dbc533965380519123c3ff46
Jenicy <46813868+Jenicy@users.noreply.github.com>
2019-02-23 21:57:28 +0800

    1-1
    第一次作业

---
+++ homework1/1-1/1-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1, i2, mul;
+            Console.WriteLine("Please input the first number:");
+            i1 = int.Parse(Console.ReadLine());
+            Console.Write("Please input the second number:");
+            i2 = int.Parse(Console.ReadLine());
+
+            mul = i1 * i2;
+            Console.WriteLine("mul=" + mul);
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
---
+++ homework1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int i1, i2, i3;
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            i1 = int.Parse(textBox1.Text);
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            i2 = int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i3 = i1 * i2;
+            textBox3.Text = Convert.ToString(i3);
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
