7fa4a7cca0934098c402f6986d79a404cd4d5a73
Jenicy <46813868+Jenicy@users.noreply.github.com>
2019-03-02 17:26:19 +0800

    第二次作业

---
+++ homework2/2-7/2-7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace _2_7
+{
+    class Program
+    {
+       
+   
+
+        static void Main(string[] args)
+        {
+            int[] array= { 34, 65, 34, 55, 66, 3, 22, 87, 54, 9, 122, 5 };
+            int max, min,  sum;
+            max = array[0];
+            min = array[0];
+            sum = 0;
+            double ave;
+            Console.WriteLine("该数组为：");
+            foreach(int a in array)
+            {
+                if(a>=max)
+                {
+                    max = a;
+                }
+                if(a<=min)
+                {
+                    min = a;
+                }
+                sum += a;
+                Console.WriteLine("{0},",a);
+            }
+            int n = array.Length;
+            ave = Convert .ToDouble(sum / n);
+            Console.WriteLine("\r\n最大值为{0}，最小值为{1},和为{2},平均值为{3}", max, min, sum, ave);
+            
+        }
+    }
+}
---
+++ homework2/2-9/2-9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flags = new int[100];
+            for(int n=0;n<100;n++)
+            {
+                flags[n] = 1;
+            }
+            flags[0] = 0;
+            flags[1] = 0;
+            int i;
+            for(i=2;i<100;++i)
+            {
+                if(flags[i]==1)
+                {
+                    for(int j=i*i;j<100;j+=i)
+                    {
+                        flags[j] = 0;
+                    }
+                    
+                }
+            }
+            Console.WriteLine("1到100 以内的素数为：");
+            for(i=2;i<100;i++)
+            {
+                if(flags[i]==1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            
+
+        }
+    }
+}
