84f408be67725e8f110ea8b8b78c1a6548c34d6c
Jenicy <46813868+Jenicy@users.noreply.github.com>
2019-03-13 21:09:31 +0800

    homework3

---
+++ homework3/FacadePattern/FacadePattern/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    class FileReader
+    {
+        public string Read(string FileNameSrc)
+        {
+            Console.WriteLine("读取文件，获取明文：");
+            FileStream fs = null;
+            StringBuilder sb = new StringBuilder();
+            fs = new FileStream(FileNameSrc, FileMode.Open);
+            int data;
+            while ((data = fs.ReadByte()) != -1)
+            {
+                sb = sb.Append((char)data);
+            }
+            fs.Close();
+            Console.WriteLine(sb.ToString());
+            return sb.ToString();
+
+        }
+    }
+
+    class CipherMachine
+    {
+        public string Encrypt(string plaintext)
+        {
+            Console.WriteLine("数据加密，将明文转化为密文：");
+            string str = " ";
+            char[] chars = plaintext.ToCharArray();
+            foreach(char ch in chars)
+            {
+                string s = (ch % 8).ToString();
+                str += s;
+
+            }
+            Console.WriteLine(str);
+            return str;
+        }
+    }
+
+    class FileWriter
+    {
+        public void Write(string encryptStr,string filename)
+        {
+            Console.WriteLine("保存密文，写入文件：");
+            FileStream fs = null;
+            fs = new FileStream(filename, FileMode.Create);
+            byte[] str = Encoding.Default.GetBytes(encryptStr);
+            fs.Write(str, 0, str.Length);
+            fs.Flush();
+            fs.Close();
+
+        }
+    }
+
+
+    class EncryptFacade
+    {
+        //维持对其他对象的引用
+        private FileReader reader;
+        private CipherMachine cipher;
+        private FileWriter writer;
+ 
+        public EncryptFacade()
+        {
+            reader = new FileReader();
+            cipher = new CipherMachine();
+            writer = new FileWriter();
+        }
+        //调用其他对象的业务方法
+        public void FileEncrypt(string fileNameSrc, string fileNameDes)
+        {
+            string plainStr = reader.Read(fileNameSrc);
+            string encryptStr = cipher.Encrypt(plainStr);
+            writer.Write(encryptStr, fileNameDes);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncryptFacade ef = new EncryptFacade();
+            ef.FileEncrypt("src.txt", "des.txt");
+            Console.Read();
+        }
+      
+        
+
+      
+    }
+}
---
+++ homework3/SimpleFactoryPattern/SimpleFactoryPattern/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryPattern
+{
+    class Program
+    {
+        interface IShape
+        {
+             void draw();
+            void erase();
+
+        }//接口
+
+        public class Circle : IShape
+        {
+            public void draw()
+            {
+                Console.WriteLine("绘制圆形");
+            }
+            public void erase()
+            {
+                Console.WriteLine("擦除圆形");
+            }
+        }
+        public class Rect : IShape
+        {
+            public void draw()
+            {
+                Console.WriteLine("绘制方形");
+
+            }
+            public void erase()
+            {
+                Console.WriteLine("擦除方形");
+            }
+
+        }
+
+        public class Triangle : IShape
+        {
+            public void draw()
+            {
+                Console.WriteLine("绘制三角形");
+                
+            }
+            public void erase()
+            {
+                Console.WriteLine("擦除三角形");
+            }
+        }
+
+        class ShapeFactory
+        {
+            public static IShape getShape(String type)
+            {
+                IShape shape=null;
+                if(type.Equals("Circle"))
+                {
+                    shape = new Circle();
+                    Console.WriteLine("初始化圆形");
+
+                }
+                else if(type.Equals("Rect"))
+                {
+                    shape = new Rect();
+                    Console.WriteLine("初始化方形");
+
+                }
+                else if(type.Equals("Triangle"))
+                {
+                    shape = new Triangle();
+                    Console.WriteLine("初始化三角形");
+                }
+                else
+                {
+                    Console.WriteLine("UnSupportedShapeException");
+                }
+
+                return shape;
+            }
+        }
+
+     
+
+        static void Main(string[] args)
+        {
+            string shape,move;
+            IShape s;
+            bool flag=false;
+            while (true)
+            {
+                Console.WriteLine("请输入要进行的操作：（start绘制并擦除图形、end退出）");
+                move = Console.ReadLine();
+                flag = false;
+                if (move == "start")
+                {
+                    while (!flag)
+                    {
+                        Console.WriteLine("请输入要绘制的图形：（c圆形、r正方形、t三角形）");
+                        shape = Console.ReadLine();
+                        switch (shape)
+                        {
+                            case "c":
+                                s = ShapeFactory.getShape("Circle");
+                                s.draw();
+                                s.erase();
+                                flag = true;
+                                break;
+                            case "r":
+                                s = ShapeFactory.getShape("Rect");
+                                s.draw();
+                                s.erase();
+                                flag = true;
+                                break;
+                            case "t":
+                                s = ShapeFactory.getShape("Triangle");
+                                s.draw();
+                                s.erase();
+                                flag = true;
+                                break;
+                            default:
+                                Console.WriteLine("请按提示正确输入！");
+                                flag = false;
+                                break;
+                        }
+                    }
+                    
+                }
+                
+                else if (move == "end")
+                {
+                    Console.WriteLine("程序结束！");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("请按提示正确输入！");
+                }
+                     
+               
+            }
+           
+       
+        }
+    }
+}
